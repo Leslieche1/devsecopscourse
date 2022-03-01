664aa425252670512121e51ac93c9bdc5596c048
Leslie Che <leslieche@Leslies-MBP.attlocal.net>
2022-02-28 22:34:09 -0600

    QODFeb28

---
+++ Feb28.py
@@ -0,0 +1,25 @@
+#!/Users/leslieche/development/devsecops/virtuale./python 
+
+Grocery = ['Milk', 'Juice', 'Snacks', 'Vegetables', 'Meat']
+
+for i in Grocery:
+    print(i)
+
+Prices = [9.45, 5.67, 3.20, 7.47, 20.23]
+
+print(Prices[0]+Prices[1]+Prices[2]+Prices[3]+Prices[4])
+
+print(Prices[0]+Prices[1]+Prices[2]+Prices[3]+ 6*(Prices[4]))
+
+str1 = 'Blood oxygentation level dependant functional magnetic resource imaging'
+print("the length of the string is :", len(str1))
+
+Fav_Food = "Achu" 
+for line in range(3):
+    result = Fav_Food * 100
+    print(result)
+
+
+
+
+
